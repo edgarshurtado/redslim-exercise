1877825a350301ed13e11b538c92f3aa6ef98622
Edgar S. Hurtado <edsanhu@gmail.com>
2026-04-27 12:40:22 +0200

    fix: remove TypeScript from jest transform scope, document cjs requirement

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,8 +1,10 @@
+// .cjs extension required: package.json has "type":"module", which makes Node treat
+// .js files as ESM. Jest needs CommonJS config, so .cjs forces CJS resolution.
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
   transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest'
+    '^.+\\.jsx?$': 'babel-jest'
   },
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
