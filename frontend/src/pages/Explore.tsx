5c4abc06c5bab7bbfa69f60a8bef8c5b3dd9d381
Edgar S. Hurtado <edsanhu@gmail.com>
2026-04-28 13:21:59 +0200

    feat: add Explore placeholder page

---
+++ frontend/src/pages/Explore.tsx
@@ -0,0 +1,15 @@
+import { Typography, Container, Box } from '@mui/material'
+
+function Explore() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ mt: 8 }}>
+        <Typography variant="h4" component="h1">
+          Explore
+        </Typography>
+      </Box>
+    </Container>
+  )
+}
+
+export default Explore
